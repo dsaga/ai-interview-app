b74ca16afa03bc1421f78843d9ba6b96778c0f6c
Dusan Petkovic <dulesaga@gmail.com>
2023-11-06 00:12:15 +0100

    fix issue with shared module not correctly bulding

--- client/functions/lib/types/index.d.ts
+++ client/functions/lib/types/index.d.ts
@@ -6,5 +6,7 @@
  *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
+export declare const callGetResults: import("firebase-functions/v2/https").CallableFunction<any, Promise<import("shared").TScoreEntity | null>>;
+export declare const callEvaluateAnswers: import("firebase-functions/v2/https").CallableFunction<any, Promise<import("shared").TPostEvaluateAnswersResponse>>;
 export declare const callGenerateQuestions: import("firebase-functions/v2/https").CallableFunction<any, Promise<import("shared").TGetQuestionsResponse>>;
 export declare const generateQuestions: import("firebase-functions/v2/https").HttpsFunction;
